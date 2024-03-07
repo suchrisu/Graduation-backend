096183a32fb1af679c4008b611cef760a7d13eec
suchrisu <1374033672@qq.com>
2024-03-07 18:05:20 +0800

    first commit

---
+++ src/test/java/com/chrisu/PolicyLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyLlmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
